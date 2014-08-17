bf49e932871eba32e595b69912afd5fa84561a05
Stefan Janecek <stefan.janecek@mathconsult.co.at>
2014-08-17 17:03:31 +0200

    use brewed git if it exists

--- zsh-plugins/git/ste-git.plugin.zsh
+++ zsh-plugins/git/ste-git.plugin.zsh
@@ -1,6 +1,11 @@
 # some additional git aliases
 
-# clustergit
+# use brewed git, if it exists
+if [[ -e /usr/local/bin/git ]]
+then
+	alias git='/usr/local/bin/git'
+fi
 
+# clustergit
 alias cgr='clustergit --recursive'
 alias cgrp='clustergit --recursive --pull'
