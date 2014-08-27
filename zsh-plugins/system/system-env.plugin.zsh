cf292d85087b9b0ffa28da43c235eecf43699825
Stefan Janecek <sjanecek@mathconsult.co.at>
2014-08-27 14:03:46 +0200

    path changes (tex, brew)

--- zsh-plugins/system/system-env.plugin.zsh
+++ zsh-plugins/system/system-env.plugin.zsh
@@ -1,5 +1,13 @@
 export EDITOR='st'
 
-export PATH=$HOME/bin:$PATH
+# /usr/local/bin needs to be in the path *before* /usr/bin
+# (because, e.g., brew otherwise fails during the upgrade of py
+# packages because it finds the native python first)
+export PATH=$HOME/bin:/usr/local/bin:$PATH
 
+# required for MacTex
+eval `/usr/libexec/path_helper -s`
+
+# some directory bookmarks
 export UNRISK_SRC=$HOME/SCM/Work/Finance/unrisk
+
