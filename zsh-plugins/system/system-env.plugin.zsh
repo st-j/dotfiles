fca6b6de0f0827aee5cbf4b8f59e2e69e85f5b65
Stefan Janecek <stefan.janecek@mathconsult.co.at>
2015-02-11 23:27:07 +0100

    added racket path

--- zsh-plugins/system/system-env.plugin.zsh
+++ zsh-plugins/system/system-env.plugin.zsh
@@ -3,7 +3,7 @@ export EDITOR='st'
 # /usr/local/bin needs to be in the path *before* /usr/bin
 # (because, e.g., brew otherwise fails during the upgrade of py
 # packages because it finds the native python first)
-export PATH=$HOME/bin:$HOME/.gem/ruby/2.0.0/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/Applications/Racket\ v6.1.1/bin/:$HOME/.gem/ruby/2.0.0/bin:/usr/local/bin:$PATH
 
 # required for MacTex
 eval `/usr/libexec/path_helper -s`
