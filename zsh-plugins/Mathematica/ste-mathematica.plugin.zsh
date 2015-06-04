2e25cff736e277119ddd4cca9d7da42ecd7efd31
Stefan Janecek <stefan.janecek@mathconsult.co.at>
2015-06-04 18:30:51 +0200

    Mathematica Script alias

---
+++ zsh-plugins/Mathematica/ste-mathematica.plugin.zsh
@@ -0,0 +1,6 @@
+
+# mma script alias
+if [[ -e /Applications/Mathematica.app/Contents/MacOS/MathematicaScript ]]
+then
+	alias mma='/Applications/Mathematica.app/Contents/MacOS/MathematicaScript -script'
+fi
